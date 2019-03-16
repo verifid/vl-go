d580ec5a1fb5c21f1c58f1bf2c9f04fd75ad6846
Abdullah Selek <abdullahselek@gmail.com>
2019-03-16 12:35:13 +0000

    Update user and add a new func for user to json.

--- vlgo/user.go
+++ vlgo/user.go
@@ -1,10 +1,24 @@
 package vlgo
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
 type User struct {
-	country      string
-	dateOfBirth  string
-	gender       string
-	name         string
-	placeOfBirth string
-	surname      string
+	Country      string `json:"country"`
+	DateOfBirth  string `json:"dateOfBirth"`
+	Gender       string `json:"gender"`
+	Name         string `json:"name"`
+	PlaceOfBirth string `json:"placeOfBirth"`
+	Surname      string `json:"surname"`
+}
+
+func UserToJson(user User) string {
+	json, err := json.Marshal(user)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(json)
 }
--- vlgo/user_test.go
+++ vlgo/user_test.go
@@ -7,6 +7,29 @@ import (
 )
 
 func TestInitStruct(t *testing.T) {
-	user := User{country: "United States", dateOfBirth: "10.04.1980", gender: "Male", name: "Tony", placeOfBirth: "New York", surname: "Stark"}
-	assert.NotNil(t, user)
+	user := User{
+		Country:      "United States",
+		DateOfBirth:  "10.04.1980",
+		Gender:       "Male",
+		Name:         "Tony",
+		PlaceOfBirth: "New York",
+		Surname:      "Stark"}
+	assert.Equal(t, user.Country, "United States")
+	assert.Equal(t, user.DateOfBirth, "10.04.1980")
+	assert.Equal(t, user.Gender, "Male")
+	assert.Equal(t, user.Name, "Tony")
+	assert.Equal(t, user.PlaceOfBirth, "New York")
+	assert.Equal(t, user.Surname, "Stark")
+}
+
+func TestUserToJson(t *testing.T) {
+	user := User{
+		Country:      "United States",
+		DateOfBirth:  "10.04.1980",
+		Gender:       "Male",
+		Name:         "Tony",
+		PlaceOfBirth: "New York",
+		Surname:      "Stark"}
+	json := UserToJson(user)
+	assert.Equal(t, json, "{\"country\":\"United States\",\"dateOfBirth\":\"10.04.1980\",\"gender\":\"Male\",\"name\":\"Tony\",\"placeOfBirth\":\"New York\",\"surname\":\"Stark\"}")
 }
