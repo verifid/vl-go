0f9fe1d25503c2ac7e1d195ba864b9a5b9c544c4
Abdullah Selek <abdullahselek@gmail.com>
2019-10-18 22:03:07 +0100

    Add sample usage file.

---
+++ samples.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"path"
+	"time"
+
+	"github.com/verifid/vl-go/vlgo"
+)
+
+func main() {
+	// vlgo client
+	client := new(vlgo.Client)
+	var httpClient = &http.Client{
+		Timeout: time.Second * 15,
+	}
+	client.HTTPClient = httpClient
+
+	// Send User Data
+	user := vlgo.User{
+		Country:     "United States",
+		DateOfBirth: "10.04.1980",
+		Name:        "Tony",
+		Surname:     "Stark"}
+	userResponse, resp, err := client.SendUserData(user)
+	fmt.Println(userResponse)
+	fmt.Println(resp)
+	fmt.Println(err)
+
+	path := path.Dir("/resources/2.png")
+	base64Str := client.ImageFileToBase64(path)
+	imageUpload := vlgo.ImageUpload{Image: base64Str, UserID: "userId"}
+	uploadResponse, resp, err := client.UploadIdentity(imageUpload, vlgo.ImageType.Identity)
+	fmt.Println(uploadResponse)
+	fmt.Println(resp)
+	fmt.Println(err)
+}
